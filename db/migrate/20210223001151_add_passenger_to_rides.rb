ac8d34597df2745cd0a9166d7fdc77b2fb9671c3
tchartchke <chartai.kl@gmail.com>
2021-02-22 18:15:56 -0600

    commit tree 904d35ea295a1d2264507c9ac7c9a47b74fb1e56

---
+++ db/migrate/20210223001151_add_passenger_to_rides.rb
@@ -0,0 +1,5 @@
+class AddPassengerToRides < ActiveRecord::Migration[5.0]
+  def change
+    add_reference :rides, :passenger, foreign_key: true
+  end
+end
---
+++ db/migrate/20210223001314_add_taxi_to_ride.rb
@@ -0,0 +1,5 @@
+class AddTaxiToRide < ActiveRecord::Migration[5.0]
+  def change
+    add_reference :rides, :taxi, foreign_key: true
+  end
+end
